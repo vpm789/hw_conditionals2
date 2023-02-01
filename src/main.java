34c3671ee5def6d886e0db9182fb5dac8a0a5be6
vpm789 <vpm789@gmail.com>
2023-02-01 22:05:57 +0300

    task1-3

--- src/main.java
+++ src/main.java
@@ -42,5 +42,13 @@ public class main {
     public static void task3() {
         System.out.println("");
         System.out.println("Задача №3");
+        int year = 1900;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            // if (year%100 != 0 || year%400 == 0) {
+            System.out.println(year + " год является високосным");
+            //  }
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
